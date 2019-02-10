ccf29af95f6122f101f63aae74b109dec8762985
rioishii <rioishii@uw.edu>
2019-02-09 16:27:10 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Rio Ishii on 2/9/19.
+//  Copyright © 2019 Rio Ishii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
